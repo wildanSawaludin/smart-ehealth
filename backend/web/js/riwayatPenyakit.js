0f0d1dcfecb0dc91d6bb498de8888268df471a45
rio <alfarioekaputra@gmail.com>
2015-03-17 05:52:07 +0700

    menambah fungsi onclick maka akan update ke table anamnesa

--- backend/web/js/riwayatPenyakit.js
+++ backend/web/js/riwayatPenyakit.js
@@ -2,8 +2,19 @@
  * Created by rio on 16/03/15.
  */
 $(document).ready(function () {
-    alert('aaa');
-    $('.select2-search').change(function(){
-        alert('aaaa');
+    $('#btnOk').click(function(){
+        var nama_penyakit = $('a span').text().split('||');
+
+        $.ajax({
+            type: "POST",
+            url: baseurl + '/anamnesa/anamnesa-riwayat/update-penyakit?id='+id,
+            data: "riwayat_penyakit_pil=1&riwayatsakit_icdx_id="+$('#icdx-id').val()+
+                    "&riwayat_penyakit_nil="+$('#anamnesa-riwayat_penyakit_nil').val()+
+                    "&riwayat_penyakit_lama="+$('#anamnesa-riwayat_penyakit_lama').val()+
+                    "&riwayat_penyakit_nama="+nama_penyakit[1],
+            success:function(data){
+                console.log(data);
+            }
+        });
     });
 });
