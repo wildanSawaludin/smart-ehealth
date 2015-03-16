a4ef051dd5fa07d14fc2813edf82f43aec2fe3c0
rio <alfarioekaputra@gmail.com>
2015-03-16 13:27:24 +0700

    menambah fungsi javascript untuk tab riwayat

--- backend/web/js/popupFunction.js
+++ backend/web/js/popupFunction.js
@@ -1,11 +1,34 @@
 $(document).ready(function () {
-       $('input[name="Anamnesa[keluhan]"]').change(function () {
-            $('#m_keluhanDetail').html('');
-            $('#m_keluhanDetail').load(baseurl + '/anamnesa/popup-keluhan?id='+id+'&param='+$(this).val());
-            $('#m_keluhanDetail').modal('show');
-//            $("#m_keluhanDetail .modal-content").css({
-//                "width": "550px",
-//                "margin": "-100px 0 0 -10%"
-//            });
+    $('input[name="Anamnesa[keluhan]"]').change(function () {
+        $('#m_keluhanDetail').html('');
+        $('#m_keluhanDetail').load(baseurl + '/anamnesa/popup-keluhan?id='+id+'&param='+$(this).val());
+        $('#m_keluhanDetail').modal('show');
+
     });
+    $('#anamnesa-riwayat_penyakit_pil').change(function(){
+        $('#m_riwayatpenyakit').html('');
+        $('#m_riwayatpenyakit').load(baseurl + '/Anamnesa/anamnesa-riwayat/popup-riwayat?id='+id+'&param='+$(this).val());
+        $('#m_riwayatpenyakit').modal('show');
+        /*$.ajax({
+         type: "POST",
+         url: baseurl + '/anamnesa/anamnesa-riwayat/update?id='+id,
+         data: "param="+$('#anamnesa-riwayat_penyakit_pil').val(),
+         success:function(data){
+         console.log(data);
+         }
+         }); */
     });
+    $('#anamnesa-riwayat_perawatan_pil').change(function(){
+        $('#m_riwayatperawatan').html('');
+        $('#m_riwayatperawatan').load(baseurl + '/Anamnesa/anamnesa-riwayat/popup-riwayat-perawatan?id='+id+'&param='+$(this).val());
+        $('#m_riwayatperawatan').modal('show');
+        /*$.ajax({
+         type: "POST",
+         url: baseurl + '/anamnesa/anamnesa-riwayat/update?id='+id,
+         data: "param="+$('#anamnesa-riwayat_penyakit_pil').val(),
+         success:function(data){
+         console.log(data);
+         }
+         }); */
+    });
+});
