401cf7bd3ed5127d3f02f4a4d6c05fb21368d518
rio <alfarioekaputra@gmail.com>
2015-03-17 21:42:44 +0700

    menambah fungsi ketika textfield icd x atau nama penyakit di klik maka akan menghapus sementara nilai di textfield tersebut

--- backend/web/js/riwayatPenyakit.js
+++ backend/web/js/riwayatPenyakit.js
@@ -2,16 +2,19 @@
  * Created by rio on 16/03/15.
  */
 $(document).ready(function () {
+    $('#kode').mousedown(function(){
+        $('#kode').val("");
+    });
+    $('#nama').mousedown(function(){
+        $('#nama').val("");
+    });
     $('#btnOk').click(function(){
-        var nama_penyakit = $('a span').text().split('||');
-
         $.ajax({
             type: "POST",
-            url: baseurl + '/anamnesa/anamnesa-riwayat/update-penyakit?id='+id,
-            data: "riwayat_penyakit_pil=1&riwayatsakit_icdx_id="+$('#icdx-id').val()+
+            url: baseurl + '/Anamnesa/anamnesa-riwayat/update-penyakit?id='+id,
+            data: "riwayat_penyakit_pil=1&riwayatsakit_icdx_id="+$('#idicdx').val()+
                     "&riwayat_penyakit_nil="+$('#anamnesa-riwayat_penyakit_nil').val()+
-                    "&riwayat_penyakit_lama="+$('#anamnesa-riwayat_penyakit_lama').val()+
-                    "&riwayat_penyakit_nama="+nama_penyakit[1],
+                    "&riwayat_penyakit_lama="+$('#anamnesa-riwayat_penyakit_lama').val(),
             success:function(data){
                 alert('Success Update Data');
                 $("#m_riwayatpenyakit").modal('hide');
