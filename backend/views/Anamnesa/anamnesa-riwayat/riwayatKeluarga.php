7bc6a480492334d1e2f061a3117cdeaac31fb3cf
rio <alfarioekaputra@gmail.com>
2015-03-25 10:55:17 +0700

    menambah script javascript

--- backend/views/Anamnesa/anamnesa-riwayat/riwayatKeluarga.php
+++ backend/views/Anamnesa/anamnesa-riwayat/riwayatKeluarga.php
@@ -80,4 +80,29 @@ use yii\helpers\Url;
     </div>
 </div>
 <script>var id = '<?php echo $_GET['id']; ?>' </script>
-<script src="/admin/js/riwayatKeluarga.js"></script>
+<!--<script src="/admin/js/riwayatKeluarga.js"></script>-->
+<script>
+    $(document).ready(function () {
+        $('#kode').mousedown(function(){
+            $('#kode').val("");
+        });
+        $('#nama').mousedown(function(){
+            $('#nama').val("");
+        });
+        $('#btnOk').click(function(){
+            var riwayat_keluarga = $("input:checkbox[name='Anamnesa[riwayat_keluarga_pil]']:checked").map(function()
+            {
+                return $(this).val();
+            }).get();
+            $.ajax({
+                type: "POST",
+                url: baseurl + '/Anamnesa/anamnesa-riwayat/update-keluarga?id='+id,
+                data: "riwayat_keluarga_pil="+riwayat_keluarga+"&riwayatkel_icdx_id="+$('#idicdx').val(),
+                success:function(data){
+                    alert('Success Update Data');
+                    $("#m_riwayatkeluarga").modal('hide');
+                }
+            });
+        });
+    });
+</script>
