4e6c5e869b5179fbd1d16c4adde1742627b96593
Teguh Iman Bastari <the.goeh.sintz@gmail.com>
2015-04-02 12:07:09 +0700

    menambahan kolom faskes_id

--- backend/models/Registrasi.php
+++ backend/models/Registrasi.php
@@ -11,32 +11,39 @@ use Yii;
  * @property string $no_reg
  * @property integer $pasienId
  * @property string $tanggal_registrasi
- * @property string $status_registrasi 
+ * @property string $status_registrasi
+ * @property string $asal_registrasi 
  * @property string $status_pelayanan
+ * @property string $tanggal_kunjungan
  * @property string $status_rawat
  * @property string $dr_penanggung_jawab
  * @property integer $icdx_id
  * @property string $status_asuransi
  * @property string $catatan
  * @property string $asuransi_noreg
+ * @property string $asuransi_noreg_other
  * @property string $asuransi_nama
  * @property string $asuransi_tgl_lahir
  * @property string $asuransi_status_jaminan
  * @property string $asuransi_penanggung_jawab
  * @property string $asuransi_alamat
  * @property string $asuransi_notelp
+ * @property integer $no_antrian 
+ * @property integer $asuransi_provider_id 
+ * @property integer $faskes_id 
  *
  * @property Anamnesa[] $anamnesas
+ * @property AsuransiProvider $asuransiProvider 
+ * @property FasilitasKesehatan $faskes 
  * @property Icdx $icdx
  * @property Pasien $pasien
  */
-class Registrasi extends \yii\db\ActiveRecord
-{
+class Registrasi extends \yii\db\ActiveRecord {
+
     /**
      * @inheritdoc
      */
-    public static function tableName()
-    {
+    public static function tableName() {
         return 'registrasi';
     }
 
@@ -45,14 +52,14 @@ class Registrasi extends \yii\db\ActiveRecord
      */
     public $pasienNama;
     public $tanggal_registrasi_format;
-    public function rules()
-    {
+
+    public function rules() {
         return [
             [['pasienId', 'status_pelayanan'], 'required'],
-            [['pasienId','asuransi_provider_id', 'icdx_id', 'no_antrian'], 'integer'],
+            [['pasienId', 'asuransi_provider_id', 'icdx_id', 'no_antrian', 'faskes_id'], 'integer'],
             [['tanggal_registrasi', 'asuransi_tgl_lahir'], 'safe'],
-            [['status_registrasi', 'status_pelayanan', 'status_rawat', 'status_asuransi', 'catatan'], 'string'],
-            [['no_reg', 'asuransi_noreg','asuransi_noreg_other', 'asuransi_notelp'], 'string', 'max' => 15],
+            [['status_registrasi', 'asal_registrasi', 'status_pelayanan', 'status_rawat', 'status_asuransi', 'catatan'], 'string'],
+            [['no_reg', 'asuransi_noreg', 'asuransi_noreg_other', 'asuransi_notelp'], 'string', 'max' => 15],
             [['dr_penanggung_jawab', 'asuransi_nama'], 'string', 'max' => 25],
             [['asuransi_status_jaminan', 'asuransi_penanggung_jawab', 'asuransi_alamat'], 'string', 'max' => 30]
         ];
@@ -61,8 +68,7 @@ class Registrasi extends \yii\db\ActiveRecord
     /**
      * @inheritdoc
      */
-    public function attributeLabels()
-    {
+    public function attributeLabels() {
         return [
             'id' => 'ID',
             'no_reg' => 'No Registrasi',
@@ -84,30 +90,40 @@ class Registrasi extends \yii\db\ActiveRecord
             'asuransi_alamat' => 'Alamat',
             'asuransi_notelp' => 'No Telepon/HP',
             'asuransi_provider_id' => 'Asuransi',
+            'faskes_id' => 'Faskes ID',
         ];
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getAnamnesas()
-    {
+    public function getAnamnesas() {
         return $this->hasMany(Anamnesa::className(), ['registrasi_id' => 'id']);
     }
 
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getIcdx()
-    {
+    public function getIcdx() {
         return $this->hasOne(Icdx::className(), ['id' => 'icdx_id']);
     }
 
     /**
-     * @return \yii\db\ActiveQuery
+     * @return \yii\db\ActiveQuery 
      */
-    public function getPasien()
-    {
+    public function getAsuransiProvider() {
+        return $this->hasOne(AsuransiProvider::className(), ['id' => 'asuransi_provider_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery 
+     */
+    public function getFaskes() {
+        return $this->hasOne(FasilitasKesehatan::className(), ['id' => 'faskes_id']);
+    }
+
+    public function getPasien() {
         return $this->hasOne(Pasien::className(), ['id' => 'pasienId']);
     }
+
 }
