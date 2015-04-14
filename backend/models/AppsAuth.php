d073662993d33e684ca4844ddbd75809120ff17e
ABED <ABED@IT-ABED-PC>
2015-04-14 11:47:10 +0700

    models AppsAuth

---
+++ backend/models/AppsAuth.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "apps_auth".
+ *
+ * @property integer $user_Id
+ * @property string $code
+ * @property integer $pasien_id
+ * @property string $created_date
+ * @property string $expired_date
+ */
+class AppsAuth extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'apps_auth';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['user_Id', 'code', 'pasien_id'], 'required'],
+            [['user_Id', 'pasien_id'], 'integer'],
+            [['created_date', 'expired_date'], 'safe'],
+            [['code'], 'string', 'max' => 32],
+            [['user_Id', 'code', 'pasien_id'], 'unique', 'targetAttribute' => ['user_Id', 'code', 'pasien_id'], 'message' => 'The combination of User  ID, Code and Pasien ID has already been taken.']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_Id' => Yii::t('app', 'User  ID'),
+            'code' => Yii::t('app', 'Code'),
+            'pasien_id' => Yii::t('app', 'Pasien ID'),
+            'created_date' => Yii::t('app', 'Created Date'),
+            'expired_date' => Yii::t('app', 'Expired Date'),
+        ];
+    }
+}
